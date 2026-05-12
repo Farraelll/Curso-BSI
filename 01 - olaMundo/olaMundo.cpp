1a1141b260181d549bc8b305ced51435e9e88b02
Rafael Marques Silva <farraelll@protonmail.com>
2026-05-12 10:39:05 -0300

    pasta errada

--- 01 - olaMundo/olaMundo.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() 
-{
-    cout << "Olá mundo!";
-    return 0;
-}
--- 01 - olaMundo/olaMundo.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-public class olaMundo {
-    public static void main(String[] args) {
-      System.out.println("Olá mundo!");
-  }
-}
